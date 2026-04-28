ec08fb182543e8d0e8eb7f4b71407757982a0965
RealNickTriano <nicholastriano1999@gmail.com>
2026-04-28 11:38:12 -0400

    deleted old frontend folder

--- frontend/src/components/OpenAiLogo.tsx
+++
@@ -1,9 +0,0 @@
-import BlackWordMarkSvg from "../assets/openai/OpenAI-black-wordmark.svg?react"
-
-export function OpenAiLogo() {
-  return (
-    <div>
-      <BlackWordMarkSvg width="100" height="50" />
-    </div>
-  )
-}
